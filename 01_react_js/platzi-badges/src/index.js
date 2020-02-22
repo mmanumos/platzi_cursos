f44147318156a27e6d129fbc3990b2e5a27b3c7c
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-02-22 17:53:20 -0500

    [Feature] Testing git modified:   src/index.js

--- 01_react_js/platzi-badges/src/index.js
+++ 01_react_js/platzi-badges/src/index.js
@@ -8,7 +8,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello platzi, from React</h1>; // It's not necessary save the HTML elements like string,
+const element = <h1>Hello platzi, Test git</h1>; // It's not necessary save the HTML elements like string,
 // because using import React from "react" i can use JSX
 const container = document.getElementById("app"); // I'm creating a variable to get an element
 
