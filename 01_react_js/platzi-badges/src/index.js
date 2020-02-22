d7f41b3a4900c148fa88711c7bc85c6027a3ab65
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-02-22 17:48:28 -0500

    [Feature] The project platzi-badges is cloned from Platzi repository.

---
+++ 01_react_js/platzi-badges/src/index.js
@@ -0,0 +1,16 @@
+// const element = document.createElement("h1");
+// element.innerText = "Hola Mundo";
+
+// const container = document.getElementById("app");
+
+// container.appendChild(element);
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello platzi, from React</h1>; // It's not necessary save the HTML elements like string,
+// because using import React from "react" i can use JSX
+const container = document.getElementById("app"); // I'm creating a variable to get an element
+
+//ReactDOM.render(__qué__, __donde__); Pseudocode
+ReactDOM.render(element, container);
